2c68bb893eb4d87f618d224c9655a30d68c99970
Ian Eddy <ianmseddy@gmail.com>
2021-06-15 16:02:18 -0700

    sourceClimateData

--- sourceClimateData.R
+++ sourceClimateData.R
@@ -81,21 +81,8 @@ sourceClimDataWholeRIA <- function(scenario, model = 'CCSM4', forFireSense = FAL
       }
 
     } else if (model == 'CanESM2') {
-      if (scenario == "RCP8.5") {
-        # ATAstack <- prepInputs(url = "https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing",
-        #                        targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.grd',
-        #                        alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.gri',
-        #                        destinationPath = path,
-        #                        filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_ATA2011-2100.grd',
-        #                        fun = 'raster::stack')
-        # CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing',
-        #                        targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.grd',
-        #                        alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.gri',
-        #                        destinationPath = path,
-        #                        filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_CMI2011-2100.grd',
-        #                        fun = 'raster::stack') #get the high quality stuff
 
-      } else if (scenario == "RCP4.5") {
+      if (scenario == "RCP4.5") {
         ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1AAR2oHX7yzAEr3UXxbgFZdL4ddDDkmCB/view?usp=sharing",
                                targetFile = 'wholeRIA_1ArcMin_CanESM2_RCP45_ATA2011-2100.grd',
                                alsoExtract = 'wholeRIA_1ArcMin_CanESM2_RCP45_ATA2011-2100.gri',
@@ -111,6 +98,19 @@ sourceClimDataWholeRIA <- function(scenario, model = 'CCSM4', forFireSense = FAL
                                filename2 = 'inputs/wholeRIA_1ArcMin_CanESM2_45_CMI2011-2100.grd',
                                fun = 'raster::stack') #get the high quality stuff
 
+      } else {
+        # ATAstack <- prepInputs(url = "https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing",
+        #                        targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.grd',
+        #                        alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_ATA2011-2100.gri',
+        #                        destinationPath = path,
+        #                        filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_ATA2011-2100.grd',
+        #                        fun = 'raster::stack')
+        # CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/17zBna_wegLmQs_m4FQd_JBhUcA0Da6lR/view?usp=sharing',
+        #                        targetFile = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.grd',
+        #                        alsoExtract = 'RIA_1ArcMinute_CanESM2_RCP85_CMI2011-2100.gri',
+        #                        destinationPath = path,
+        #                        filename2 = 'inputs/RIA_1ArcMinute_CanESM2_85_CMI2011-2100.grd',
+        #                        fun = 'raster::stack') #get the high quality stuff
       }
     } else if (model == 'CNRM-CM5') {
       if (scenario == 'RCP4.5') {
@@ -159,18 +159,18 @@ sourceClimDataWholeRIA <- function(scenario, model = 'CCSM4', forFireSense = FAL
                                filename2 = 'inputs/wholeRIA_1ArcMin_CSIRO-Mk3_CMI2011-2100.grd',
                                fun = 'raster::stack') #get the high quality stuff
       } else {
-        # ATAstack <- prepInputs(url = "https://drive.google.com/file/d/1SZ8zDH5H3frLIiwXa8M6t0W15Rueh9Nf/view?usp=sharing",
-        #                        targetFile = 'RIA_1ArcMinute_CSIRO_mk3_ATA2011-2100.grd',
-        #                        alsoExtract = 'RIA_1ArcMinute_CSIRO_mk3_ATA2011-2100.gri',
-        #                        destinationPath = path,
-        #                        filename2 = 'inputs/RIA_1ArcMinute_CSIRO_mk3_45_ATA2011-2100.grd',
-        #                        fun = 'raster::stack')
-        # CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/1SZ8zDH5H3frLIiwXa8M6t0W15Rueh9Nf/view?usp=sharing',
-        #                        targetFile = 'RIA_1ArcMinute_CSIRO_mk3_CMI2011-2100.grd',
-        #                        alsoExtract = 'RIA_1ArcMinute_CSIRO_mk3_CMI2011-2100.gri',
-        #                        destinationPath = path,
-        #                        filename2 = 'inputs/RIA_1ArcMinute_CSIRO_mk3_45_CMI2011-2100.grd',
-        #                        fun = 'raster::stack')
+        ATAstack <- prepInputs(url = "https://drive.google.com/file/d/11kuhp-CZ4kTYhm4a0sSSPHPZPejW87Vk/view?usp=sharing",
+                               targetFile = 'wholeRIA_20kmbuffer_CSIRO_mk3_ATA2011-2100.grd',
+                               alsoExtract = 'wholeRIA_20kmbuffer_CSIRO_mk3_ATA2011-2100.gri',
+                               destinationPath = path,
+                               filename2 = 'wholeRIA_1ArcMin_CSIRO_Mk3_RC85_ATA2011-2100.grd',
+                               fun = 'raster::stack')
+        CMIstack <- prepInputs(url = 'https://drive.google.com/file/d/11kuhp-CZ4kTYhm4a0sSSPHPZPejW87Vk/view?usp=sharing',
+                               targetFile = 'wholeRIA_20kmbuffer_CSIRO_mk3_CMI2011-2100.grd',
+                               alsoExtract = 'wholeRIA_20kmbuffer_CSIRO_mk3_CMI2011-2100.gri',
+                               destinationPath = path,
+                               filename2 = 'wholeRIA_1ArcMin_CSIRO_Mk3_RC85_CMI2011-2100.grd',
+                               fun = 'raster::stack')
       }
     } else if (model == 'Access1') {
       if (scenario == 'RCP4.5') {
@@ -213,16 +213,21 @@ sourceClimDataWholeRIA <- function(scenario, model = 'CCSM4', forFireSense = FAL
   } else {
     #put fireSense links
     urls = list(
-      "CSIRO-Mk3" = list("RCP8.5" = 'https://drive.google.com/file/d/1qZnHSHzdeBk_CvG67XNYQNLU-7BMEAMx/view?usp=sharing',
-                         "RCP4.5" = 'https://drive.google.com/file/d/1wA-bpR4DSE_O0oYvXfLa63wLx-RAazYE/view?usp=sharing'),
-      "Access1" = list("RCP8.5" = NA,
-                       "RCP4.5" = 'https://drive.google.com/file/d/14MgMibS7RQUOiq3wD34NaXq-m1E-wL0u/view?usp=sharing'),
+      "CSIRO-Mk3" = list("RCP8.5" = list(url = 'https://drive.google.com/file/d/1qZnHSHzdeBk_CvG67XNYQNLU-7BMEAMx/view?usp=sharing',
+                                         filename = "wholeRIA_CSIRO-Mk3_RCP85_projMDC2011_2100"),
+                         "RCP4.5" = list(url = 'https://drive.google.com/file/d/1wA-bpR4DSE_O0oYvXfLa63wLx-RAazYE/view?usp=sharing',
+                                         filename = "wholeRIA_CSIRO-Mk3_RCP45_projMDC2011_2100")),
+      "Access1" = list("RCP8.5" = list(url = 'https://drive.google.com/file/d/14MgMibS7RQUOiq3wD34NaXq-m1E-wL0u/view?usp=sharing',
+                                       filename = "wholeRIA_Access1_RCP85_projMDC2011_2100"),
+                       "RCP4.5" = list(url = 'https://drive.google.com/file/d/14MgMibS7RQUOiq3wD34NaXq-m1E-wL0u/view?usp=sharing',
+                                       filename = "wholeRIA_Access1_RCP45_projMDC2011_2100")),
       "INM-CM4" = list("RCP8.5" = list(url = "https://drive.google.com/file/d/1oDYpesRUHp8v1QzCwMT5op9kYxdEMiKC/view?usp=sharing",
                                        filename = "wholeRIA_INM-CM4_RCP85_projMDC2011_2100"),
                        "RCP4.5" = list(url = "https://drive.google.com/file/d/1C_iYUHV7IsfC4nLSHf6jhHFZN0ylFPhK/view?usp=sharing",
                                        filename = "wholeRIA_INM-CM4_RCP45_projMDC2011_2100")),
-      "CanESM2" = list("RCP8.5" = NA,
-                       "RCP4.5" = "https://drive.google.com/file/d/1NvXFe6yoNxDsnVnvVYk3xoG6vqoQCgQD/view?usp=sharing")
+      "CanESM2" = list("RCP8.5" = list(NA),
+                       "RCP4.5" = list(url = "https://drive.google.com/file/d/1NvXFe6yoNxDsnVnvVYk3xoG6vqoQCgQD/view?usp=sharing",
+                                       filename = "wholeRIA_CanESM2_RCP45_projMDC2011_2100"))
     )
     theUrl <- urls[[model]][[scenario]][["url"]]
     theFile1 <- paste0(urls[[model]][[scenario]][["filename"]], ".grd")
