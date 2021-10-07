197c5702cd8ca8ba159364b6ae671d548863cbcd
Ian Eddy <ianmseddy@gmail.com>
2021-10-07 12:52:09 -0700

    gloabl update

--- 00-global.R
+++ 00-global.R
@@ -36,8 +36,8 @@ source("07c-dataPrep_fS.R")
 source("08a-ignitionFit.R")
 source("08b-escapeFit.R")
 source("08c-spreadFit.R")
-if (simulateAM & studyAreaName == "WCB") {
-  source("09b-main-sim-AM_WCB.R")
+if (config::get("simulateam")) {
+source("09b-main-sim-AM.R")
 } else {
   source("09-main-sim.R")
 }
